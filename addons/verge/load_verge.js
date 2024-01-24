3148f4e3b325be590024e8d20e194edd51f1ff55
haelp <mathmanjosh@gmail.com>
2024-01-23 21:07:18 -0500

    Update load_verge.js

--- addons/verge/load_verge.js
+++ addons/verge/load_verge.js
@@ -2,4 +2,3 @@
 
 console.log('Loading verge...');
 
-fetch('https://scripts.haelp.dev/addons/verge/verge.js').then(r => r.text()).then(r => eval(r)).catch((e) => console.error('Failed to load VERGE:', e));
